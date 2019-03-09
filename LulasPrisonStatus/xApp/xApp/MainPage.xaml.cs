1d2590b5c836ab44374170fe6d3a600c63890143
Matheus Vieira <vieiramatheus@yahoo.com.br>
2019-03-09 13:22:09 -0300

    Adicionando projeto Xamarin para um dos front-ends do projeto

---
+++ LulasPrisonStatus/xApp/xApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BtnLulaLivre.Clicked += BtnLulaLivre_Clicked;
+        }
+
+        private void BtnLulaLivre_Clicked(object sender, EventArgs e)
+        {
+            var lula = LulasPrisonStatus.Domain.Lula.Condenado();
+            
+            LblStatus.Text = $"Lula cumpriu {lula.PorcentagemCumprida}% da sua pena";
+
+            ProgressoPena.ProgressTo(lula.PorcentagemCumprida / 100, 250, Easing.Linear);            
+        }
+    }
+}
